d0e9c4af7b3f18455b01e5f0308c693178dd672d
Mooho Lee <dlangh6632@naver.com>
2024-03-21 10:23:57 +0900

    feature: 프로필 사진 업로드 및 보여주기 구현

--- src/pages/member/Mypage.js
+++ src/pages/member/Mypage.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
-import { Grid, Container, Typography, TextField, Button, Link, Box, Checkbox } from '@mui/material';
+import React, { useState, useRef, useEffect } from 'react'
+import { Grid, Container, Typography, TextField, Button, Box, Avatar } from '@mui/material';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import axios from 'axios';
 
 function a11yProps(index) {
     return {
@@ -18,6 +19,8 @@ const Mypage = () => {
     let passwordCheckVal = false;
 
     const [value, setValue] = React.useState(0);
+    const [profileImage, setImage] = useState(null);
+    const [userNickname, setUserNickname] = useState(null);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -35,6 +38,58 @@ const Mypage = () => {
         setshowPwCheck(!showPwCheck);
     };
 
+    const handleClick = () => {
+        thumbnailInput.current.click();
+      };
+    
+
+    const thumbnailInput = useRef();
+
+    const handleProfileImage = useEffect(async e => {
+        try { await axios.get(`http://localhost:9090/mypage`, 
+                    {
+                        headers: {
+                            Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
+                        }
+                    })
+            .then((response) => {
+                setImage(response.data.item.profileFile);
+                setUserNickname(response.data.item.userNickname);
+            })
+            .catch((error) => {
+                console.error('Error fetching image', error);
+            });
+        } catch (error) {
+            console.warn("이미지 불러오기 실패", error);
+        }
+    }, []);
+
+    const saveFileImage = async e => {
+        try {
+            const formData = new FormData();
+            formData.append('profile_image', e.target.files[0]);
+            
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`,
+                    'Content-Type': 'multipart/form-data'
+                },
+            };
+            try {
+            const response = await axios.post(
+            `http://localhost:9090/mypage/profile-file`, 
+            formData, 
+            config
+            ); 
+        } catch (error) {
+                console.warn(error);
+            }
+        window.location.reload();
+        } catch (error) {
+            console.warn("이미지 업로드 실패");
+        }
+      };
+
   return (
     <Container component="main" maxWidth="xs" style={{ marginTop: '8%', maxWidth:'1300px' }}>
     
@@ -47,7 +102,7 @@ const Mypage = () => {
           <Tab label="즐겨찾기" {...a11yProps(2)} />
           <Tab label="구매내역" {...a11yProps(3)} />
           <Tab label="포인트" {...a11yProps(4)} />
-          <Tab label="멤버십" {...a11yProps(5)} />
+          <Tab label="수료증" {...a11yProps(5)} />
           <Tab label="장바구니" {...a11yProps(6)} />
           <Tab label="알림설정" {...a11yProps(7)} />
         </Tabs>
@@ -63,25 +118,22 @@ const Mypage = () => {
                 </Grid>
                 <Grid container xs={12} style={{justifyContent: 'center', textAlign: 'center'}}>
                     <Grid item xs={6}>
-                        <Typography component="h1" variant="h5" style={{textAlign: 'center', marginTop: '5%'}}>
-                            프로필
-                        </Typography>
                         <Box item xs={10}>
-                            <img src="https://source.unsplash.com/random" alt="profile" style={{width: '80%', height: '200px', borderRadius: '40%'}}/>
+
+                        {profileImage === null ? (
+                            <Avatar src="/broken-mage.jpg" style={{width: '200px', height: '200px', borderRadius: '70%'}}/> 
+                            ) : (
+                            <img src={`https://kr.object.ncloudstorage.com/envdev/`+profileImage} alt='thumbnail' style={{width: '200px', height: '200px', borderRadius: '70%'}}/> 
+                            )}
                         </Box>
-                        <Button type="submit" fullWidth variant="contained" color="primary" style={{height:'55px', width: '10rem', fontSize:'18px', marginBottom: '15%'}}>
-                            사진 변경
-                        </Button>
                     </Grid>
                     <Grid item xs={6}>
-                        <Typography component="h1" variant="h5" style={{textAlign: 'center', marginTop: '5%'}}>
-                            뱃지
+                        <Typography component="h1" variant="h5" style={{textAlign: 'center', marginTop: '15%'}}>
+                            프로필
                         </Typography>
-                        <Box item xs={10}>
-                            <img src="https://source.unsplash.com/random" alt="profile" style={{width: '80%', height: '200px', borderRadius: '40%'}}/>
-                        </Box>
-                        <Button type="submit" fullWidth variant="contained" color="primary" style={{height:'55px', width: '10rem', fontSize:'18px', marginBottom: '15%'}}>
-                            뱃지 변경
+                        <Button onClick={handleClick} variant="contained" color="primary" style={{height:'55px', width: '10rem', fontSize:'18px', marginTop: '15%'}}>
+                            사진 변경
+                            <input type='file' accept='image/jpg, image/jpeg, image/png' multiple ref={thumbnailInput} onChange={saveFileImage} style={{ display: 'none' }} />
                         </Button>
                     </Grid>
                 </Grid>
@@ -138,12 +190,9 @@ const Mypage = () => {
                         </Button>
                     </Grid>
                     <Grid item xs={12}>
-                        <Typography component="p" variant='string' style={{textAlign: 'left'}}>
+                        <Typography component="h2" variant='string' style={{textAlign: 'left'}}>
                         '강사 계정으로 전환하기' 신청을 합니다.
                         </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Checkbox onChange={(e) => {agreementCheckVal = !agreementCheckVal}}/> 동의합니다.
                     </Grid>
                     <Grid item xs={12}>
                         <Button type="submit" fullWidth variant="contained" color="primary" style={{height:'55px', fontSize:'18px', marginBottom: '15%'}}>
