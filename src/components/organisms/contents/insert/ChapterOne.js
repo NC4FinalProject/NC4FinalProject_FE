b820ef11bf9ab44b6b1e103202c26cf5ae070b64
azorur <azorur159@gmail.com>
2024-03-27 12:38:42 +0900

    Feat: 등록페이지_강의명에서 콘텐츠 명으로 변경, 커리큘럼 버튼 활성 시 조작 버튼 활성화되도록 수정

--- src/components/organisms/contents/insert/ChapterOne.js
+++ src/components/organisms/contents/insert/ChapterOne.js
@@ -3,7 +3,17 @@ import React from 'react'
 import UploadImage from './UploadImage'
 import UploadVideo from './UploadVideo'
 import CoTypography from '../../../atoms/common/CoTypography'
+import styled from 'styled-components'
 
+const CustomTextField = styled(TextField)({
+  '& .MuiInput-input::placeholder': {
+    fontSize: '0.9rem', // 원하는 글자 크기로 조정
+  },
+  '& .MuiInput-input': {
+    textAlign: 'center', // 입력 필드 텍스트를 수평 중앙 정렬
+    // 필요하다면 lineHeight도 조정할 수 있음
+  },
+});
 
 const ChapterOne = () => {
   return (
@@ -12,10 +22,10 @@ const ChapterOne = () => {
         <Grid item xs={2} />
 
         <Grid item xs={1} style={{ display: 'flex', alignItems: 'center' }}>
-            <CoTypography size="Title">강의명</CoTypography>
+            <CoTypography size="Title">컨텐츠</CoTypography>
         </Grid>
         <Grid item xs={6}>
-          <TextField fullWidth id="standard-basic" variant="standard" />
+          <CustomTextField fullWidth id="standard-basic" variant="standard" placeholder='컨텐츠 제목을 입력하세요.'/>
         </Grid>
         <Grid item xs={3} />
     </Grid>
