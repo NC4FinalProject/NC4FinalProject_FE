e6e0f3d5d0b5066263bd222a9e93f8432965e508
AZORURCLIENT\AzorUrClient <azorur159@gmail.com>
2024-04-01 08:32:19 +0900

    feat: 컨텐츠 관련 api 추가

--- src/components/api/ContentsApi.js
+++ src/components/api/ContentsApi.js
@@ -1,71 +1,47 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getContents = createAsyncThunk(
-    'board/getContents',
-    async(search, thunkAPI) => {
-        try{
-            const response = await axios.get(
-                `http://localhost:9090/contents/listAll`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                    },
-                    // params: {
-                    //     searchCondition: search.searchCondition,
-                    //     searchKeyword: search.searchKeyword,
-                    //     page: search.page
-                    // }
-                }
-            );
-            
-            return response.data;
-        }catch(e){
-            return thunkAPI.rejectWithValue(e);
-        }
+export async function fetchContentsApi() {
+
+    const token = sessionStorage.getItem("ACCESS_TOKEN");
+    console.log("Sending token: ", token);
+
+    try {
+        const response = await axios.get(`http://localhost:9090/contents/listAll`, {
+            // headers: {
+            //     Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
+            // }
+            // params: {
+            //     searchCondition: search.searchCondition,
+            //     searchKeyword: search.searchKeyword,
+            //     page: search.page
+            // }
+        });
+        // console.log(response.headers); // 모든 응답 헤더를 콘솔에 출력
+        // console.log(response.headers['content-type']); // 예를 들어, 특정 헤더 'Content-Type' 확인하기
+        return response.data;
+    } catch (error) {
+        throw error;
     }
-);
+}
 
-
-
-
-
-export const postBoard = createAsyncThunk(
-    'board/postBoard',
-    async(board, thunkAPI) =>{
-        try{
-            const response = await axios.post(
-                `http://localhost:9090/board/board`,board,{
-                    headers: {
-                        Authorization:`Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`,
-                        "Content-Type":"multipart/form-data"
-                    }
-                }
-            );
-            return response.data.pageItems;
-        } catch(e) {
-            return thunkAPI.rejectWithValue(e);
-        }
+export const ChapterOneApiSave = async (chapterOne) => {
+    try {
+      const response = await axios.post('http://localhost:9090/contents/listAll', 
+      chapterOne,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
+    //     },
+    //     params: {
+    //       searchCondition: search.searchCondition,
+    //       searchKeyword: search.searchKeyword,
+    //       page: search.page
+    //     }
+    //   }
+    );
+      return response.data;
+    } catch (error) {
+      throw error;
     }
-)
-
-export const removeBoard = createAsyncThunk(
-    'board/removeBoard',
-    async(boardNo, thunkAPI) => {
-        try{
-            const response = await axios.delete(
-                `http://localhost:9090/board/board/${boardNo}`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                    }
-                }
-            );
-            
-            return response.data.pageItems;
-
-        }catch(e){
-            return thunkAPI.rejectWithValue(e);
-        }
-    }
-);
+  };
+  
