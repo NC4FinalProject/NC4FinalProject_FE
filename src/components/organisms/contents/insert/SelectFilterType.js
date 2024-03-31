db8ed690c6386541f2faba887cdb592f5f81346f
AZORURCLIENT\AzorUrClient <azorur159@gmail.com>
2024-04-01 08:31:49 +0900

    feat: 전역상태관리로 인한 챕터2 관련 컴포넌트 대거 수정

---
+++ src/components/organisms/contents/insert/SelectFilterType.js
@@ -0,0 +1,156 @@
+import { Grid, MenuItem, TextField } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import CoTypography from '../../../atoms/common/CoTypography'
+import styled from 'styled-components';
+import { chapterOneStore, contentsCategoryItems, contentsPriceTypeItems, useChapterOneStore } from '../../../../stores/ContentsStore';
+
+const CustomTextField = styled(TextField)({
+  '& .MuiInput-input::placeholder': {
+    fontSize: '0.9rem', // 원하는 글자 크기로 조정
+  },
+  '& .MuiInput-input': {
+    textAlign: 'center', // 입력 필드 텍스트를 수평 중앙 정렬
+    // 필요하다면 lineHeight도 조정할 수 있음
+  },
+});
+
+// 분야 유형 또는 가격 유형에 따른 컴포넌트 표출
+const FilterMap = ({isType, placeholder, handleTypeSelector, payType}) => {
+
+  const { chapterOne, chapterOneInput } = useChapterOneStore();
+
+  const valueType = isType ? chapterOne.category : chapterOne.priceType;
+  // const getValueType = () => isType ? chapterOne.category : chapterOne.priceType;
+  
+  return(
+    <CustomTextField
+      select
+      value={valueType} 
+      variant="standard" 
+      onChange={handleTypeSelector}
+      fullWidth
+      defaultValue=""
+
+      label={placeholder}
+      InputLabelProps={{
+        style: { textAlign: 'center', width: '100%' },
+      }}
+    >
+      
+      {
+        isType ? (
+          contentsCategoryItems.map((el) => (
+            <MenuItem key={el.id} value={el.type}>
+              {el.type}
+            </MenuItem>
+          ))
+        ):(
+          contentsPriceTypeItems.map((el) => (
+            <MenuItem key={el.id} value={el.type} >
+              {el.type}
+            </MenuItem>
+          ))
+        )
+      }
+
+    </CustomTextField>
+  )
+
+}
+
+const SelectFilterType = ({title, placeholder, isType}) => {
+
+  const [payType, setPayType] = useState("");
+
+  const [pay, setPay] = useState("");
+
+  const { chapterOne, chapterOneInput } = useChapterOneStore();
+
+  const [formattedPrice, setFormattedPrice] = useState('');
+  
+  // 가격 정보 상태 저장
+  const handlePriceChange = (e) => {
+    // chapterOneInput({ price: e.target.value });
+
+    const rawPrice = e.target.value.replace(/[^0-9]/g, '');
+    const formattedValue = rawPrice ? new Intl.NumberFormat('de-DE').format(parseInt(rawPrice, 10)) : '';
+
+    // 포맷된 값을 뷰 상태로 저장
+    setFormattedPrice(formattedValue);
+
+    // 숫자만을 저장하는 로직
+    chapterOneInput({ price: rawPrice });
+  };
+
+  /// 카테고리 및 가격 유형 정보 상태 저장
+  const handleTypeSelector = (e) => {
+    setPayType(e.target.value);
+    if(isType){
+      chapterOneInput({ category: e.target.value });
+      // setValueType(chapterOne.category)
+      console.log("카테고리 유형 선택 함수에는 오니?")
+    } else {
+      chapterOneInput({ priceType: e.target.value });
+      // setValueType(chapterOne.priceType)
+    }
+  };
+
+  // 유료 강의에서 무료 강의 시, 가격 초기화 및 컨텐츠 가격 유형 결정
+  useEffect((e) => {
+    if (payType === "무료 강의") {
+      // console.log("무료 강의 타입으로 눌림")
+      setPay("0")
+      chapterOneInput({ price: ""})
+    } else if (payType === '유료 강의'){
+      // console.log("유료 강의 타입으로 눌림")
+      setPay("1")
+
+    } else if (payType === '국비 지원') {
+      // console.log("국비 지원 타입으로 눌림")
+      setPay("-1")
+      chapterOneInput({ price: ""})
+    }  
+    // console.log(payType)
+  }, [payType]);
+
+  // 가격 포맷
+  useEffect(() => {
+    const rawPrice = chapterOne.price;
+    const formattedValue = rawPrice ? new Intl.NumberFormat('de-DE').format(parseInt(rawPrice, 10)) : '';
+    setFormattedPrice(formattedValue);
+  }, [chapterOne.price]);
+
+  return (
+  <Grid container justifyContent="center" sx={{ marginTop: '0.35rem', }} >
+    <Grid item xs={2} />
+
+    <Grid item xs={1} style={{ display: 'flex', alignItems: 'end', justifyContent:'center', paddingBottom: '0.1rem'}}>
+        <CoTypography size="Title">{title}</CoTypography>
+    </Grid>
+
+    {/* 유료강의 일 경우 가격입력 란 표시 */}
+    <Grid item xs={6}> 
+    {!isType && chapterOne.priceType === "유료 강의" ? (
+      <Grid container alignItems={'flex-end'}>
+        <Grid item xs={8} >
+          <CustomTextField fullWidth id="standard-basic" variant="standard" 
+            value={formattedPrice || ""} 
+            onChange={handlePriceChange}  
+            placeholder='가격을 입력하세요.'/>
+        </Grid>
+        <Grid item xs={4}>
+          <FilterMap isType={isType} placeholder={placeholder} handleTypeSelector={handleTypeSelector} payType={payType}/>
+        </Grid>
+      </Grid>
+    ) : (
+      <FilterMap isType={isType} placeholder={placeholder} handleTypeSelector={handleTypeSelector} payType={payType}/>
+    )}
+    </Grid>
+    
+    <Grid item xs={3} />
+  </Grid>
+
+  )
+}
+
+export default SelectFilterType
---
+++ src/stores/ContentsStore.js
@@ -0,0 +1,119 @@
+import { create } from 'zustand';
+import { ChapterOneApiSave, fetchContentsApi } from '../components/api/ContentsApi';
+
+// test
+export const useContentsStore = create((set) => ({
+  contents: [],
+  fetchContents: async () => {
+      try {
+          const data = await fetchContentsApi();
+          set({ contents: data });
+      } catch (error) {
+          console.error(error);
+      }
+  }
+}));
+
+// 가격타입
+export const contentsPriceTypeItems = [
+  { id: "el1", type: "무료 강의" },
+  { id: "el2", type: "유료 강의" },
+  { id: "el3", type: "국비 지원" },
+];
+
+// 카테고리 
+export const contentsCategoryItems = [
+  { id: "el1", type: "개발 · 프로그래밍" },
+  { id: "el2", type: "게임 개발" },
+  { id: "el3", type: "데이터 사이언스" },
+  { id: "el4", type: "인공지능" },
+  { id: "el5", type: "보안 · 네트워크" },
+  { id: "el6", type: "비즈니스 · 마케팅" },
+  { id: "el7", type: "인공지능" },
+  { id: "el8", type: "보안 · 네트워크" },
+  { id: "el9", type: "하드웨어" },
+  { id: "el10", type: "웹 디자인" },
+];
+
+// 챕터 1에 대한 입력 정보 상태 및 액션
+export const useChapterOneStore = create(set => ({
+  chapterOne: {
+    category: '',
+    priceType: '',
+    price: '',
+    contentsTitle: '',
+  },
+  chapterOneInput: (newContents) => set(state => ({
+    chapterOne: { ...state.chapterOne, ...newContents }
+  })),
+  saveChapterOne: async (chapterOneDetails) => {
+    const data = await ChapterOneApiSave(chapterOneDetails);
+    set({ chapterOne: data });
+  },
+}));
+
+// 챕터 2에 대한 입력 정보 상태 및 액션
+export const useChapterTwoStore = create((set) => ({
+  chapterTwo: [],
+
+  // 독립적인 메인 섹션 입력 단 & 아이디 값 부여
+  sectionTitleInput: (index, newTitle) => set((state) => {
+    const sectionTitleInput = state.chapterTwo.map((chapterTwoChange, idx) => {
+      if (idx === index) {
+        return { ...chapterTwoChange, sectionTitle: newTitle, sectionId: index};
+      }
+      return chapterTwoChange;
+    });
+    return { chapterTwo: sectionTitleInput };
+  }),
+  // 독립적인 서브 섹션 입력 단 & 아이디 값 부여
+  sectionSubTitleInput: (sectionSubId, sectionId, newSubTitle) => set((state) => {
+    const chapterTwoUpdated = state.chapterTwo.map((section) => {
+      const sectionSubListUpdated = section.sectionSubList.map((subSection) => {
+        if (subSection.sectionSubId === sectionSubId) {
+          return { ...subSection, sectionSubTitle: newSubTitle };
+        }
+        return subSection;
+      });
+      return { ...section, sectionSubList: sectionSubListUpdated };
+    });
+    return { ...state, chapterTwo: chapterTwoUpdated };
+  }),
+  // 메인 섹션 객체 추가
+  addSection: (newSection) => set((state) => ({
+    chapterTwo: [...state.chapterTwo, newSection]
+  })),
+  // 메인 섹션 객체 삭제
+  removeSection: (sectionId) => set((state) => ({
+    chapterTwo: state.chapterTwo.filter(section => section.sectionId !== sectionId)
+  })),
+  // 서브 섹션 객체 추가
+  addSectionSub: (sectionId, newSectionSub) => set((state) => {
+    console.log("addSectionSub 호출됨");
+    console.log("전달된 sectionId:", sectionId);
+    console.log("추가될 newSectionSub:", newSectionSub);
+  
+    const updatedChapterTwo = state.chapterTwo.map((section) => {
+      console.log("현재 섹션의 sectionId:", section.sectionId);
+      if (section.sectionId === sectionId) {
+        console.log("일치하는 섹션 발견:", section);
+        return { ...section, sectionSubList: [...section.sectionSubList, newSectionSub] };
+      } else {
+        return section;
+      }
+    });
+  
+    console.log("업데이트된 chapterTwo:", updatedChapterTwo);
+    return { chapterTwo: updatedChapterTwo };
+  }),
+  // 서브 섹션 객체 삭제
+  removeSectionSub: (sectionSubId) => set((state) => ({
+    chapterTwo: state.chapterTwo.map(section => {
+      // 각 섹션의 sectionSubList에서 해당하는 하위 섹션 ID를 제외하고 필터링
+      const newSectionSubList = section.sectionSubList.filter(subSection => subSection.sectionSubId !== sectionSubId);
+      // 필터링된 sectionSubList로 각 섹션을 업데이트
+      return { ...section, sectionSubList: newSectionSubList };
+    })
+  })),
+
+}));
