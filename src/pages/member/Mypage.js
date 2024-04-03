8f9b5504177c6099ad6f0fc603528b8233ca6af0
Mooho Lee <dlangh6632@naver.com>
2024-04-03 17:05:43 +0900

    feat: 마이페이지 탭메뉴 분류

--- src/pages/member/Mypage.js
+++ src/pages/member/Mypage.js
@@ -1,11 +1,36 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { Grid, Container, Typography, TextField, Button, Box, Avatar } from '@mui/material';
+import React, { useState } from 'react'
+import PropTypes from "prop-types";
+import { Container, Box } from '@mui/material';
+import Typography from "@mui/material/Typography";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { MoonLoader} from "react-spinners";
-import { roles } from 'aria-query';
+import MemberInfo from '../../components/organisms/mypage/MemberInfo';
+
+function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography component={"div"}>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
 
 function a11yProps(index) {
     return {
@@ -14,267 +39,87 @@ function a11yProps(index) {
     };
   }
 
-
 const Mypage = () => {
 
-    const navi = useNavigate();
-
+    
     const [value, setValue] = useState(0);
-    const [profileImage, setImage] = useState(null);
-    const [role, setRole] = useState(null);
-    const [userNickname, setUserNickname] = useState(null);
-    const [loading, setLoading] = useState(true);
+
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
-    const [showPw, setshowPw] = useState(false);
-
-    const toggleShowPw = () => {
-        setshowPw(!showPw);
-      };
-
-    const [showPwCheck, setshowPwCheck] = useState(false);
-
-    const toggleShowPwCheck = () => {
-        setshowPwCheck(!showPwCheck);
-    };
-
-    const handleClick = () => {
-        thumbnailInput.current.click();
-      };
-    
-
-    const thumbnailInput = useRef();
-
-    useEffect(async e => {
-        try { 
-            const response = await axios.get(`http://localhost:9090/mypage`, 
-                    {
-                        headers: {
-                            Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                        }
-                    });
-            
-                setImage(response.data.item.profileFile);
-                setUserNickname(response.data.item.userNickname);
-                setLoading(false);
-                setRole(response.data.item.role);
-        } catch (error) {
-            console.warn("이미지 불러오기 실패", error);
-            sessionStorage.removeItem("ACCESS_TOKEN");
-            navi('/');
-        }
-    }, []);
-
-    if (loading) {
-        return (
-        <Grid Container marginBottom='30%' marginTop='10%'style={{ position: 'flex'}} >
-            <Grid item xs={12} style={{ position: 'absolute', left: '50%'}}>
-                <MoonLoader color="#558BCF" />
-            </Grid>
-            
-        </Grid> 
-        )
-    }
-
-    const saveFileImage = async e => {
-        try {
-            const formData = new FormData();
-            formData.append('profile_image', e.target.files[0]);
-            
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`,
-                    'Content-Type': 'multipart/form-data'
-                },
-            };
-            try {
-            const response = await axios.post(
-            `http://localhost:9090/mypage/profile-file`, 
-            formData, 
-            config
-            ); 
-        } catch (error) {
-                console.warn(error);
-            }
-        window.location.reload();
-        } catch (error) {
-            console.warn("이미지 업로드 실패");
-        }
-      };
-
-      const handleOnChange = (e) => {
-        setUserNickname(e.target.value);
-      }
-
-    
-    const updateUserNickname = async e => {
-        try {
-            const formData = new FormData();
-            formData.append('user_nickname', userNickname);
-
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`,
-                    'Content-Type': 'multipart/form-data'
-                }
-            };
-
-            try {
-                const response = await axios.post(
-                    `http://localhost:9090/mypage/user-nickname`, 
-                    formData, 
-                    config
-                ); 
-            } catch (error) {
-                console.warn(error);
-                if (error.response.data.errorCode === 202) {
-                    alert("이미 존재하는 닉네임 입니다.");
-                    }
-            }
-            } catch (error) {
-                console.warn("닉네임 변경 실패");
-                }   
-      };
-
-      const wannabeTeacher = async e => {
-        try {
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                }
-            };
-
-            try {
-                const response = await axios.get(
-                    `http://localhost:9090/mypage/wannabe-teacher`, 
-                    config
-                ); 
-                alert("강사 신청 완료");
-            } catch (error) {
-                console.warn(error);
-                if (error.response.data.errorCode === 202) {
-                    alert("이미 강사 입니다.");
-                    }
-            }
-            } catch (error) {
-                console.warn("강사 신청 실패");
-                }   
-      };
-
-      const resign = async e => {
-
-                try {
-                    const response = await axios.delete(
-                        `http://localhost:9090/member/resign`, 
-                        {
-                            headers: {
-                                Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                            }
-                        }
-                    )
-
-                    const response2 = await axios.get(
-                        `http://localhost:9090/member/logout`, 
-                        {
-                            headers: {
-                                Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                            }
-                        }
-                    )
-                    sessionStorage.removeItem("ACCESS_TOKEN");
-                    window.alert("회원탈퇴 성공.");
-                    navi("/");
-
-                } catch (error) {
-                        alert("회원탈퇴하실 수 없습니다.");
-                }
-      };
-
-
   return (
     <Container component="main" maxWidth="xs" style={{ marginTop: '8%', maxWidth:'1300px' }}>
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="계정정보" {...a11yProps(0)} />
-          <Tab label="수강목록" {...a11yProps(1)} />
-          <Tab label="즐겨찾기" {...a11yProps(2)} />
-          <Tab label="구매내역" {...a11yProps(3)} />
-          <Tab label="포인트" {...a11yProps(4)} />
-          <Tab label="수료증" {...a11yProps(5)} />
-          <Tab label="장바구니" {...a11yProps(6)} />
-          <Tab label="알림설정" {...a11yProps(7)} />
-        </Tabs>
-      </Box>
-    </Box>
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="계정정보" {...a11yProps(0)} />
+                <Tab label="수강목록" {...a11yProps(1)} />
+                <Tab label="즐겨찾기" {...a11yProps(2)} />
+                <Tab label="구매내역" {...a11yProps(3)} />
+                <Tab label="포인트" {...a11yProps(4)} />
+                <Tab label="수료증" {...a11yProps(5)} />
+                <Tab label="장바구니" {...a11yProps(6)} />
+                <Tab label="알림설정" {...a11yProps(7)} />
+                </Tabs>
+            </Box>
 
-        <form>
-            <Grid container spacing={2} sx={{width: '40%', marginLeft: '30%'}}>
-                <Grid item xs={12}>
-                    <Typography component="h1" style={{textAlign: 'center', marginTop: '5%', fontSize:'3rem'}}>
-                        계정 정보
+            {/* 계정정보 */}
+
+            <div
+                role="tabpanel"
+            >
+                
+                <Box sx={{ p: 3 }}>
+                    <Typography component={"div"}>
+                        <MemberInfo/>
                     </Typography>
-                </Grid>
-                <Grid container xs={12} style={{justifyContent: 'center', textAlign: 'center'}}>
-                    <Grid item xs={6}>
-                        <Box item xs={10}>
-                        {profileImage === null ? (
-                            <Avatar src="/broken-mage.jpg" style={{width: '200px', height: '200px', borderRadius: '70%', marginLeft: '15%'}}/> 
-                            ) : (
-                            <img src={`https://kr.object.ncloudstorage.com/bitcamp-bucket-36/`+profileImage} alt='thumbnail' style={{width: '200px', height: '200px', borderRadius: '70%'}}/> 
-                            )}
-                        </Box>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Typography component="h1" variant="h5" style={{textAlign: 'center', marginTop: '15%'}}>
-                            프로필
-                        </Typography>
-                        <Button onClick={handleClick} variant="contained" color="primary" style={{height:'55px', width: '10rem', fontSize:'18px', marginTop: '15%'}}>
-                            사진 변경
-                            <input type='file' accept='image/jpg, image/jpeg, image/png' multiple ref={thumbnailInput} onChange={saveFileImage} style={{ display: 'none' }} />
-                        </Button>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12}>
-                        <TextField
-                            name="userNickname"
-                            variant="outlined"
-                            fullWidth
-                            id="userNickname"
-                            label="닉네임"
-                            defaultValue={ userNickname === null ? ("닉네임을 입력해주세요.") : (userNickname)}
-                            onChange={handleOnChange}
-                        ></TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button type="button" onClick={updateUserNickname} fullWidth variant="contained" color="primary" style={{height:'55px', fontSize:'18px', marginBottom: '5%'}}>
-                            닉네임 변경하기
-                        </Button>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Typography component="h2" variant='string' style={{textAlign: 'left'}}>
-                            { role === "ROLE_TEACHER" ? ("이미 강사입니다."
-                                ) : ( 
-                                    "아직 강사가 아닙니다.") }
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button type="submit" onClick={wannabeTeacher} fullWidth variant="contained" color="primary" style={{height:'55px', fontSize:'18px', marginBottom: '15%'}}>
-                            강사 계정으로 전환 신청하기
-                        </Button>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button type="submit" onClick={resign} fullWidth variant="contained" color="gray" style={{height:'55px', fontSize:'18px', marginBottom: '15%'}}>
-                            회원탈퇴
-                        </Button>
-                    </Grid>
-            </Grid>
-        </form>
+                </Box>
+                
+            </div>
+            {/* <CustomTabPanel value={value} index={0}>
+                <MemberInfo/>
+            </CustomTabPanel> */}
+
+            {/* 수강목록 */}
+            <CustomTabPanel value={value} index={1}>
+                수강목록
+            </CustomTabPanel>
+
+            {/* 즐겨찾기 */}
+            <CustomTabPanel value={value} index={2}>
+                즐겨찾기
+            </CustomTabPanel>
+
+            {/* 구매목록 */}
+            <CustomTabPanel value={value} index={3}>
+                구매목록
+            </CustomTabPanel>
+
+            {/* 포인트 */}
+            <CustomTabPanel value={value} index={4}>
+                포인트
+            </CustomTabPanel>
+
+            {/* 수료증 */}
+            <CustomTabPanel value={value} index={5}>
+                수료증
+            </CustomTabPanel>
+
+            {/* 장바구니 */}
+            <CustomTabPanel value={value} index={6}>
+                장바구니
+            </CustomTabPanel>
+
+            {/* 알림설정 */}
+            <CustomTabPanel value={value} index={7}>
+                알림설정
+            </CustomTabPanel>
+        </Box>
     </Container>
-  )
-}
+   )
+ }
+
 
 export default Mypage
