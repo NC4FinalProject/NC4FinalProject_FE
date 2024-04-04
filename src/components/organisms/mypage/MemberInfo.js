568a812bb4bf09c9dad7342cdaf53ada1bc667cb
Mooho Lee <dlangh6632@naver.com>
2024-04-04 14:42:07 +0900

    fix: 프로필 사진 변경

--- src/components/organisms/mypage/MemberInfo.js
+++ src/components/organisms/mypage/MemberInfo.js
@@ -199,7 +199,7 @@ const MemberInfo = () => {
                         {profileImage === null ? (
                             <Avatar src="/broken-mage.jpg" style={{width: '200px', height: '200px', borderRadius: '70%', marginLeft: '15%'}}/> 
                             ) : (
-                            <img src={`https://kr.object.ncloudstorage.com/bitcamp-bucket-36/`+profileImage} alt='thumbnail' style={{width: '200px', height: '200px', borderRadius: '70%'}}/> 
+                            <img src={`https://kr.object.ncloudstorage.com/envdev/`+profileImage} alt='thumbnail' style={{width: '200px', height: '200px', borderRadius: '70%'}}/> 
                             )}
                         </Box>
                     </Grid>
