d38820ab280ddfe76fed27f8de73b7e10cdf20c4
misu <jagalji@daum.net>
2024-04-09 17:29:42 +0900

    Feat : memberInfo persist session storage 사용해 상태 관리 구현

--- src/stores/MemberStore.js
+++ src/stores/MemberStore.js
@@ -1,8 +1,18 @@
 import create from 'zustand';
+import {persist} from 'zustand/middleware';
 
-const MemberStore = create((set, get) => ({
+const MemberStore = create(persist((set, get) => ({
     userRole: 'null',
     setUserRole: (userRole) => set({userRole}),
+    memberInfo: {
+        memberId: "",
+        username: "",
+        userNickname: "",
+    },
+    setMemberInfo: (memberInfo) => set({memberInfo}),
+}), {
+    name: 'member-store',
+    getStorage: () => sessionStorage,
 }));
 
 export default MemberStore;
