2b98f736991e92d8f7556ec6979760f9bc69c2ce
misu <jagalji@daum.net>
2024-04-09 14:21:22 +0900

    Feat : login 후 userInfo context에 등록 기능 구현 중

--- src/components/organisms/review/Reportdialog.js
+++ src/components/organisms/review/Reportdialog.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useContext, useState} from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
@@ -6,153 +6,148 @@ import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import CoTypography from "../../atoms/common/CoTypography";
-import CoSelect from "../common/CoSelect";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  TextField,
-} from "@mui/material";
+import {Table, TableBody, TableCell, TableRow, TextField,} from "@mui/material";
+import {MemberInfoContext} from "../../../pages/member/MemberInfoContext";
 
-const Reportdialog = ({ open, handleClickClose, children, selectComponent, author, date, Title, onSubmit }) => {
-  const [reportReason, setReportReason] = useState("");
-  const [detailReason, setDetailReason] = useState("");
+const Reportdialog = ({open, handleClickClose, children, selectComponent, author, date, Title, onSubmit}) => {
+    const [reportReason, setReportReason] = useState("");
+    const [detailReason, setDetailReason] = useState("");
+    const {setUserInfo} = useContext(MemberInfoContext);
 
-  const handleReportChange = (e) => {
-    console.log(e);
-    setReportReason(e.target.value);
-  };
+    const handleReportChange = (e) => {
+        console.log(e);
+        setReportReason(e.target.value);
+    };
 
-  const handleReasonChange = (event) => {
-    console.log(event.target.value);
-    setDetailReason(event.target.value);
-  };
+    const handleReasonChange = (event) => {
+        console.log(event.target.value);
+        setDetailReason(event.target.value);
+    };
 
-  const handleCancel = () => {
-    console.log(reportReason);
-    console.log(detailReason);
-    setReportReason("");
-    setDetailReason("");
-    handleClickClose();
-  };
+    const handleCancel = () => {
+        console.log(reportReason);
+        console.log(detailReason);
+        setReportReason("");
+        setDetailReason("");
+        handleClickClose();
+    };
 
-  const handleSubmit = () => {
-    onSubmit(detailReason);
-    console.log(reportReason);
-    console.log(detailReason);
-    setReportReason("");
-    setDetailReason("");
-    handleClickClose();
-  };
+    const handleSubmit = () => {
+        onSubmit(detailReason);
+        console.log(reportReason);
+        console.log(detailReason);
+        setReportReason("");
+        setDetailReason("");
+        handleClickClose();
+    };
 
-  return (
-    <Dialog
-      open={open}
-      onClose={(event, detailReason) => {
-        if (detailReason !== "backdropClick") {
-          handleClickClose(event);
-        }
-      }}
-      disableEscapeKeyDown
-    >
-      <DialogTitle style={{ marginTop: "0.5rem", marginBottom: "0.5rem" }}>
-        <CoTypography size="DialogTitle">{Title}</CoTypography>
-      </DialogTitle>
-      <DialogContent
-        style={{
-          width: "28.125rem",
-          height: "27rem",
-          padding: "1rem",
-          margin: "0 auto",
-        }}
-      >
-        <Box
-          style={{
-            width: "95%",
-            maxWidth: "26rem",
-            background: "#EFEFEF",
-            padding: "0.625rem",
-            margin: "0 auto",
-            borderRadius: "0.25rem",
-          }}
-        >
-          <CoTypography size="Content" style={{ marginBottom: "35px" }}>
-          {children || '1. 허위신고는 처벌 대상입니다.'}
-          </CoTypography>
-        </Box>
-        <Box sx={{ margin: "0.5rem auto 0", maxWidth: "27rem" }}>
-        {selectComponent}
-        </Box>
-        <Table
-          sx={{
-            borderCollapse: "collapse",
-            borderStyle: "hidden",
-            margin: "0.5rem auto 0",
-            width: "100%",
-            maxWidth: "27rem",
-            borderRadius: "0.25rem",
-            boxShadow: "inset 0 0 0 1px #E0E0E0",
-            textAlign: "center",
-          }}
-        >
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                <CoTypography size="Content">
-                  작성자 : {author} <br />
-                  작성일 : {date}
-                </CoTypography>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                <TextField
-                  multiline
-                  rows={6}
-                  value={detailReason}
-                  onChange={handleReasonChange}
-                  fullWidth
-                  variant="standard"
-                  placeholder="상세적인 내용을 작성해주세요."
-                  required
-                  sx={{
-                    width: "100%",
-                  }}
-                  InputProps={{ disableUnderline: true }}
-                />
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </DialogContent>
-      <DialogActions>
-        <Box
-          sx={{
-            justifyContent: "center",
-            width: "100%",
-            display: "flex",
-            transform: "translateY(-60%)",
-          }}
-        >
-          <Button
-            onClick={handleCancel}
-            variant="outlined"
-            style={{
-              marginRight: "1.25rem",
-              color: "black",
-              borderColor: "#ced4da",
+    return (
+        <Dialog
+            open={open}
+            onClose={(event, detailReason) => {
+                if (detailReason !== "backdropClick") {
+                    handleClickClose(event);
+                }
             }}
-          >
-            취소하기
-          </Button>
-          <Button onClick={handleSubmit} color="error" variant="contained">
-            {Title === "블랙리스트 추가 / 변경" ? "변경하기" : "삭제하기"}
-          </Button>
-        </Box>
-      </DialogActions>
-    </Dialog>
-  );
+            disableEscapeKeyDown
+        >
+            <DialogTitle style={{marginTop: "0.5rem", marginBottom: "0.5rem"}}>
+                <CoTypography size="DialogTitle">{Title}</CoTypography>
+            </DialogTitle>
+            <DialogContent
+                style={{
+                    width: "28.125rem",
+                    height: "27rem",
+                    padding: "1rem",
+                    margin: "0 auto",
+                }}
+            >
+                <Box
+                    style={{
+                        width: "95%",
+                        maxWidth: "26rem",
+                        background: "#EFEFEF",
+                        padding: "0.625rem",
+                        margin: "0 auto",
+                        borderRadius: "0.25rem",
+                    }}
+                >
+                    <CoTypography size="Content" style={{marginBottom: "35px"}}>
+                        {children || '1. 허위신고는 처벌 대상입니다.'}
+                    </CoTypography>
+                </Box>
+                <Box sx={{margin: "0.5rem auto 0", maxWidth: "27rem"}}>
+                    {selectComponent}
+                </Box>
+                <Table
+                    sx={{
+                        borderCollapse: "collapse",
+                        borderStyle: "hidden",
+                        margin: "0.5rem auto 0",
+                        width: "100%",
+                        maxWidth: "27rem",
+                        borderRadius: "0.25rem",
+                        boxShadow: "inset 0 0 0 1px #E0E0E0",
+                        textAlign: "center",
+                    }}
+                >
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                <CoTypography size="Content">
+                                    작성자 : {author} <br/>
+                                    작성일 : {date}
+                                </CoTypography>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <TextField
+                                    multiline
+                                    rows={6}
+                                    value={detailReason}
+                                    onChange={handleReasonChange}
+                                    fullWidth
+                                    variant="standard"
+                                    placeholder="상세적인 내용을 작성해주세요."
+                                    required
+                                    sx={{
+                                        width: "100%",
+                                    }}
+                                    InputProps={{disableUnderline: true}}
+                                />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </DialogContent>
+            <DialogActions>
+                <Box
+                    sx={{
+                        justifyContent: "center",
+                        width: "100%",
+                        display: "flex",
+                        transform: "translateY(-60%)",
+                    }}
+                >
+                    <Button
+                        onClick={handleCancel}
+                        variant="outlined"
+                        style={{
+                            marginRight: "1.25rem",
+                            color: "black",
+                            borderColor: "#ced4da",
+                        }}
+                    >
+                        취소하기
+                    </Button>
+                    <Button onClick={handleSubmit} color="error" variant="contained">
+                        {Title === "블랙리스트 추가 / 변경" ? "변경하기" : "삭제하기"}
+                    </Button>
+                </Box>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 export default Reportdialog;
---
+++ src/pages/member/MemberInfoContext.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+export const MemberInfoContext = React.createContext();
+
+export const MemberInfoProvider = ({children}) => {
+    const [userInfo, setUserInfo] = useState({
+        memberId: "",
+        username: "",
+        userNickname: "",
+    });
+
+    return (
+        <MemberInfoContext.Provider value={userInfo}/>
+    );
+};
